6ba335a7688129e228b0d7e6502d42a560ec98a8
Marc Basquens <marc5@kth.se>
2023-11-15 14:22:05 +0100

    A

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,8 +2,8 @@ using BenchmarkTools
 using FiniteLineSource
 using Plots
 
-suite = BenchmarkGroup()
-suite["seg_to_seg"] = @benchmarkable begin
+SUITE = BenchmarkGroup()
+SUITE["seg_to_seg"] = @benchmarkable begin
     s1 = BoreholeSegment(0, 0, 0.5, 2, 0.1)
     s2 = BoreholeSegment(0, 1, 1, 5, 0.1)
 
@@ -12,7 +12,7 @@ suite["seg_to_seg"] = @benchmarkable begin
     result = response.(t)
 end
 
-suite["T_field"] = @benchmarkable begin
+SUITE["T_field"] = @benchmarkable begin
     s = BoreholeSegment(0, 0, 0.5, 2, 0.1)
     points = [(i, j) for i in 0:0.1:2, j in 0.1:0.1:2]
     points = T_ls.(last.(points), first.(points), 10, Ref(s))
