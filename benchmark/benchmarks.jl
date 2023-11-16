09833c171af8fd39c2a7806b52c6ea2978cc471d
Marc Basquens <marc5@kth.se>
2023-11-16 12:10:30 +0100

    A

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,21 +4,6 @@ using BenchmarkTools
 export SUITE
 
 const SUITE = BenchmarkGroup()
-#=SUITE["seg_to_seg"] = @benchmarkable begin
-    s1 = BoreholeSegment(0, 0, 0.5, 2, 0.1)
-    s2 = BoreholeSegment(0, 1, 1, 5, 0.1)
-
-    t = 0:10^3:10^7
-    response(x) = segment_to_segment_step_response(x, source=s1, receptor=s2)
-    result = response.(t)
-end
-
-SUITE["T_field"] = @benchmarkable begin
-    s = BoreholeSegment(0, 0, 0.5, 2, 0.1)
-    points = [(i, j) for i in 0:0.1:2, j in 0.1:0.1:2]
-    points = T_ls.(last.(points), first.(points), 10, Ref(s))
-end
-=#
 
 SUITE["utf8"] = BenchmarkGroup(["string", "unicode"])
 
