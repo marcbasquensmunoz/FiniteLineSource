e8eadef09cd5e717098f4d181b6c56011066b56c
Marc Basquens <marc5@kth.se>
2024-03-15 10:50:12 +0100

    Organize plots code

--- figures/error_fls.jl
+++ figures/error_fls.jl
@@ -1,13 +1,15 @@
 using FiniteLineSource
-using FiniteLineSource: convolve_step
+using FiniteLineSource: convolve_fls_step
 using WGLMakie
 using CairoMakie
 
-function produce_plot(r, segment_points, segment_limits, label, color)        
+function produce_plot_fls(r, segment_points, label)        
     q = [20*sin(2π*i/8760) + 5*sin(2π*i/24) + 5. for i=1:8760*20]
     I = zeros(length(q))
 
-    setup = PointToPoint(r=r)
+    setup = SegmentToPoint(D=5., H=50., r=r, z=30.)
+
+    segment_limits = [0., 0.01, 0.1, 0.5, 1., 3., 10.]
 
     params = Constants(Δt = 3600., segment_limits=segment_limits, segment_points=segment_points)
     prealloc = Preallocation(setup, params) 
@@ -15,30 +17,26 @@ function produce_plot(r, segment_points, segment_limits, label, color)
     precomp = precompute_parameters(setup, prealloc=prealloc, params=params)
     compute_integral_throught_history!(setup, I=I, q=q, precomp=precomp, params=params)
 
-    C = convolve_step(q, Δt = params.Δt, r = setup.r)
+    C = convolve_fls_step(q, Δt=3600., r=setup.r, z=setup.z, D=setup.D, H=setup.H, α=params.α, kg=params.kg)
 
     abs_error = abs.(C-I) 
     t = 1:length(q)
     abs_error[abs_error .== 0] .= 10^-32
     error = log10.(abs_error)
 
-    lines!(t, error, color=color)#, label=label, ylimits=(-20, 0))
+    lines!(t, error, label=label)
 end
-
 
 rs = [0.1, 1, 10, 100]
 plots = []
 ff = Figure(size = (600,600))
 
-segment_limits_1 = [0., 10.]
-segment_limits = [0., 0.01, 0.1, 0.5, 1., 3., 10.]
-
 for (i, r) in enumerate(rs)
     ax = Axis(ff[floor(Int, (i-1)/2) + 1, (i-1)%2+1], xlabel = "t (h)", ylabel = "log₁₀(ϵ)", title="r̃=$(Int(r/0.1))")
-    produce_plot(r, [50], segment_limits_1, "n=50", :blue)    
-    produce_plot(r, [100], segment_limits_1, "n=100", :red)       
-    produce_plot(r, [200], segment_limits_1, "n=200", :green)       
-    produce_plot(r, [400], segment_limits_1, "n=400", :orange)     
-    produce_plot(r, [600], segment_limits_1, "n=600", :black)         
+    produce_plot_fls(r, [5, 20, 5, 5, 5, 5], "∑n=45")    
+    produce_plot_fls(r, 1 .* [10, 25, 10, 10, 10, 10], "∑n=75")       
+    produce_plot_fls(r, 2 .* [10, 25, 10, 10, 10, 10], "∑n=150")       
+    produce_plot_fls(r, 3 .* [10, 25, 10, 10, 10, 10], "∑n=225")     
+    produce_plot_fls(r, 4 .* [10, 25, 10, 10, 10, 10], "∑n=300")       
 end
 ff
