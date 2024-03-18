5eee284ee2a668821418a4a4cc11c6a8af64a511
Marc Basquens <marc5@kth.se>
2024-03-18 10:57:35 +0100

    Improve plots

--- figures/error_fls.jl
+++ figures/error_fls.jl
@@ -3,15 +3,16 @@ using FiniteLineSource: convolve_fls_step
 using WGLMakie
 using CairoMakie
 
-function produce_plot_fls(r, segment_points, label)        
+function produce_plot_fls(r, line_points, label)        
     q = [20*sin(2π*i/8760) + 5*sin(2π*i/24) + 5. for i=1:8760*20]
     I = zeros(length(q))
 
     setup = SegmentToPoint(D=5., H=50., r=r, z=30.)
 
     segment_limits = [0., 0.01, 0.1, 0.5, 1., 3., 10.]
+    segment_points = 4 .* [10, 25, 10, 10, 10, 10]
 
-    params = Constants(Δt = 3600., segment_limits=segment_limits, segment_points=segment_points)
+    params = Constants(Δt = 3600., segment_limits=segment_limits, segment_points=segment_points, line_points=line_points)
     prealloc = Preallocation(setup, params) 
 
     precomp = precompute_parameters(setup, prealloc=prealloc, params=params)
@@ -24,19 +25,23 @@ function produce_plot_fls(r, segment_points, label)
     abs_error[abs_error .== 0] .= 10^-32
     error = log10.(abs_error)
 
-    lines!(t, error, label=label)
+    lines!(t ./ 8760, error, label=label)
 end
 
 rs = [0.1, 1, 10, 100]
 plots = []
-ff = Figure(size = (600,600))
+ff = Figure(size = (800,600))
 
 for (i, r) in enumerate(rs)
-    ax = Axis(ff[floor(Int, (i-1)/2) + 1, (i-1)%2+1], xlabel = "t (h)", ylabel = "log₁₀(ϵ)", title="r̃=$(Int(r/0.1))")
-    produce_plot_fls(r, [5, 20, 5, 5, 5, 5], "∑n=45")    
-    produce_plot_fls(r, 1 .* [10, 25, 10, 10, 10, 10], "∑n=75")       
-    produce_plot_fls(r, 2 .* [10, 25, 10, 10, 10, 10], "∑n=150")       
-    produce_plot_fls(r, 3 .* [10, 25, 10, 10, 10, 10], "∑n=225")     
-    produce_plot_fls(r, 4 .* [10, 25, 10, 10, 10, 10], "∑n=300")       
+    ax = Axis(ff[floor(Int, (i-1)/2) + 1, (i-1)%2+1], xlabel = "t (years)", ylabel = "log₁₀(ϵ)", title="r̃=$(Int(r/0.1))", limits=(nothing, (-20, 0)))
+    produce_plot_fls(r, 200, "N=200")    
+    produce_plot_fls(r, 400, "N=400")       
+    produce_plot_fls(r, 600, "N=600")       
+    produce_plot_fls(r, 800, "N=800")     
+    produce_plot_fls(r, 1000, "N=1000")       
 end
+ff[2, 3] = Legend(ff, ax, "")
 ff
+
+CairoMakie.activate!()
+save("figures/error_fls.pdf", ff)
--- figures/errors_ps.jl
+++ figures/errors_ps.jl
@@ -3,7 +3,7 @@ using FiniteLineSource: convolve_step
 using WGLMakie
 using CairoMakie
 
-function produce_plot(r, segment_points, segment_limits, label, color)        
+function produce_plot(r, segment_points, segment_limits, label)        
     q = [20*sin(2π*i/8760) + 5*sin(2π*i/24) + 5. for i=1:8760*20]
     I = zeros(length(q))
 
@@ -22,23 +22,55 @@ function produce_plot(r, segment_points, segment_limits, label, color)
     abs_error[abs_error .== 0] .= 10^-32
     error = log10.(abs_error)
 
-    lines!(t, error, color=color)#, label=label, ylimits=(-20, 0))
+    lines!(t ./ 8760, error, label=label)
 end
 
+####
+## Plots with m=1
 
 rs = [0.1, 1, 10, 100]
 plots = []
-ff = Figure(size = (600,600))
+ff = Figure(size = (800,600))
 
-segment_limits_1 = [0., 10.]
+segment_limits = [0., 10.]
+
+for (i, r) in enumerate(rs)
+    ax = Axis(ff[floor(Int, (i-1)/2) + 1, (i-1)%2+1], xlabel = "t (years)", ylabel = "log₁₀(ϵ)", title="r̃=$(Int(r/0.1))", limits=(nothing, (-20, 0)))
+    produce_plot(r, [50], segment_limits, "n=50")    
+    produce_plot(r, [100], segment_limits, "n=100")       
+    produce_plot(r, [200], segment_limits, "n=200")       
+    produce_plot(r, [400], segment_limits, "n=400")     
+    produce_plot(r, [600], segment_limits, "n=600")         
+end
+ff[2, 3] = Legend(ff, ax, "")
+ff
+
+CairoMakie.activate!()
+save("figures/error_m1.pdf", ff)
+##
+
+
+
+####
+## Plots with m=6
+
+rs = [0.1, 1, 10, 100]
+plots = []
+ff = Figure(size = (800,600))
+
 segment_limits = [0., 0.01, 0.1, 0.5, 1., 3., 10.]
 
 for (i, r) in enumerate(rs)
-    ax = Axis(ff[floor(Int, (i-1)/2) + 1, (i-1)%2+1], xlabel = "t (h)", ylabel = "log₁₀(ϵ)", title="r̃=$(Int(r/0.1))")
-    produce_plot(r, [50], segment_limits_1, "n=50", :blue)    
-    produce_plot(r, [100], segment_limits_1, "n=100", :red)       
-    produce_plot(r, [200], segment_limits_1, "n=200", :green)       
-    produce_plot(r, [400], segment_limits_1, "n=400", :orange)     
-    produce_plot(r, [600], segment_limits_1, "n=600", :black)         
+    ax = Axis(ff[floor(Int, (i-1)/2) + 1, (i-1)%2+1], xlabel = "t (years)", ylabel = "log₁₀(ϵ)", title="r̃=$(Int(r/0.1))", limits=(nothing, (-20, 0)))
+    produce_plot(r, [10, 15, 5, 5, 5, 5], segment_limits, "∑n=45")    
+    produce_plot(r, [10, 25, 10, 10, 10, 10], segment_limits, "∑n = 75")       
+    produce_plot(r, 2 .* [10, 25, 10, 10, 10, 10], segment_limits, "∑n = 150")       
+    produce_plot(r, 3 .* [10, 25, 10, 10, 10, 10], segment_limits, "∑n = 225")     
+    produce_plot(r, 4 .* [10, 25, 10, 10, 10, 10], segment_limits, "∑n = 300")       
 end
+ff[2, 3] = Legend(ff, ax, "")
 ff
+
+CairoMakie.activate!()
+save("figures/error_m6.pdf", ff)
+##
