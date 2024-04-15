0dfe35a2c248c52bc8fbe7337bf8d3f1414e629c
Marc Basquens <marc5@kth.se>
2024-04-15 13:42:02 +0200

    Implement adaptive integration

--- src/integration.jl
+++ src/integration.jl
@@ -5,3 +5,31 @@ function integrate(f, a, b, n)
     @. x = x * m + c
     m * dot(f.(x), w)
 end
+
+function integrate_x_w(f, a, b, n)
+    x, w = gausslegendre(n+1)
+    m = (b-a)/2
+    c = (b+a)/2
+    @. x = x * m + c
+    @. w = m * w
+    dot(f.(x), w), x, w
+end
+
+function adaptive_integration(f, a, b, tol, n = 3)
+    m = (a+b)/2   
+
+    I = integrate(f, a, b, n)
+    I1, x1, w1 = integrate_x_w(f, a, m, n)
+    I2, x2, w2 = integrate_x_w(f, m, b, n)
+
+    I_sub = I1 + I2
+    error = abs(I - I_sub)
+
+    if error > tol 
+        In1, xn1, wn1 = adaptive_integration(f, a, m, tol)
+        In2, xn2, wn2 = adaptive_integration(f, m, b, tol)
+        return In1 + In2, [xn1; xn2], [wn1; wn2]
+    else
+        return I_sub, [x1; x2], [w1; w2]
+    end
+end
--- src/self_response.jl
+++ src/self_response.jl
@@ -1,12 +1,23 @@
 
 function compute_self_response(n; t, rb, H, α, kg)
     σ = rb
+
+    x, w = gausslegendre(n+1)
+
     params = MeanSegToSegEvParams(D1=0., H1=H, D2=0., H2=H, σ=σ)
     h_mean_sts, r_min, r_max = mean_sts_evaluation(params)
 
     principal(r) = erfc(σ/sqrt(4*t*α)) / (2π*kg*sqrt(2*σ*(r-σ)))
     f(r) = h_mean_sts(r) * point_step_response(t, r, α, kg) - principal(r)
     principal_integral = sqrt(r_max - r_min) * erfc(r_min/sqrt(4α*t)) / (kg*π*sqrt(2*r_min))
+    b = 5*rb
 
-    integrate(f, r_min, r_max, n) + principal_integral
+    integrate(f, r_min, b, x, w) + integrate(f, b, r_max, x, w) + principal_integral
+    #integrate(f, r_min, b, n) + integrate(f, b, r_max, n) + principal_integral
+end
+
+function integrate(f, a, b, x, w)
+    m = (b-a)/2
+    c = (b+a)/2
+    m * dot(w, @. f(m*x+c))
 end
