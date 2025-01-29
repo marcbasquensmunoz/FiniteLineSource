7bc115332b0d3f1a1062eebc4885ba850020d7d8
Marc Basquens <marc5@kth.se>
2025-01-29 14:00:55 +0100

    Improve allocations

--- examples/blocks/benchmark_line_precomp.jl
+++ examples/blocks/benchmark_line_precomp.jl
@@ -33,6 +33,22 @@ compute_distance(x, y) = sqrt((x[1] - y[1])^2 + (x[2] - y[2])^2)
 
 params = Constants(Δt=Δt, α=α, kg=kg, rb=rb)
 
+struct BlockMethod{T <: Number}
+    ζ::Vector{T}
+    F::Vector{T}
+    expt::Vector{T}
+    expNin::Vector{T}
+    expNout::Vector{T}
+    HM::Array{T, 3}
+    load_delays::Vector{Queue{T}}
+    load_buffer::Queue{T}
+    ranges::Vector{UnitRange{Int}}
+    Kranges::Vector{UnitRange{Int}}
+    K_min::Matrix{Int}
+    qinaux::Vector{T}
+    qoutaux::Vector{T}
+end
+
 function prepare_containers(bh_positions, D, H, z_eval, ϵ, params, nmodel)
     @unpack Δt, α, rb = params
     Δt̃ = Δt*α/rb^2
@@ -65,116 +81,89 @@ function prepare_containers(bh_positions, D, H, z_eval, ϵ, params, nmodel)
     load_delays = [Queue{Float64}() for _ in eachindex(F)]
     load_buffer = Queue{Float64}()
 
-    for j in 1:N[1]
+    for _ in 1:N[1]
         enqueue!(load_buffer, 0.)
     end
     for i in 1:length(load_delays)-1
-        for j in 1:N[i+1]-N[i]
+        for _ in 1:N[i+1]-N[i]
             enqueue!(load_delays[i], 0.)
         end
     end
 
-    #HM = Vector{Vector{HMatrix{ClusterTree{2, Float64}, Float64}}}[]
     bins = [10, 20, 30, 40, 50, 60, 70, 80, 100, 125, 150, 175, 200, 250]
     X, _, Fx, PP, XT = create_bin_containers(bins)
 
-    #=
-    function G(x, y, k)
-        if x == y return 1. end
-        σ = sqrt((x[1]-y[1])^2 + (x[2]-y[2])^2)
-        setup = SegmentToPoint(D=D, H=H, z=z_eval, σ=σ)
-        lineparams = LineKernelParams(setup, k/rb, ϵ, eval)
-        bin1 = FiniteLineSource.get_bin(lineparams.n1, bins)
-        bin2 = FiniteLineSource.get_bin(lineparams.n2, bins)
-        compute_kernel_line(lineparams; x1=X[bin1], x2=X[bin2], X1=XT[bin1], X2=XT[bin2], P1=PP[bin1], P2=PP[bin2], f1=Fx[bin1], f2=Fx[bin2])
-    end=#
-    #allζ = reduce(vcat, ζ)
-    #HM = [zeros(length(bh_positions), length(bh_positions), length(ζζ)) for ζζ in ζ]
-    HM = [[zeros(length(bh_positions), length(bh_positions)) for ζζ in ζ[i]] for i in eachindex(ζ)]
+    distances = [compute_distance(source, target) for source in bh_positions, target in bh_positions]
+    NR = [σ == 0 ? 0. : compute_N_line(σ, D, H, z_eval, ϵ, params) for σ in distances]
+    K_min = [N_r == 0 ? 0 : findlast(x -> x <= N_r, N) for N_r in NR]
+    indices = cumsum([length(F[i]) for i in eachindex(F)])
+    insert!(indices, 1, 0)
+    ranges = [indices[i]+1:indices[i+1] for i in eachindex(indices[1:end-1])]
+    Kranges = [index+1:indices[end] for index in indices[1:end-1]]
 
-    for (l, vζ) in enumerate(ζ), (k, ζζ) in enumerate(vζ), (j, target) in enumerate(bh_positions), i in 1:j-1
+    ζflat = reduce(vcat, ζ)
+    Wflat = reduce(vcat, W)
+    Fflat = reduce(vcat, F)
+    exptflat = reduce(vcat, expt)
+    expNinflat = reduce(vcat, expNin)
+    expNoutflat = reduce(vcat, expNout)
+
+    HMflat = zeros(length(ζflat), length(bh_positions), length(bh_positions))
+
+    C = 1 / (2π^2*kg)
+
+    for (k, ζζ) in enumerate(ζflat), (j, target) in enumerate(bh_positions), i in 1:j-1
         σ = compute_distance(bh_positions[i], target)
         setup = SegmentToPoint(D=D, H=H, z=z_eval, σ=σ)
         lineparams = LineKernelParams(setup, ζζ/rb, ϵ, nmodel)
         bin1 = FiniteLineSource.get_bin(lineparams.n1, bins)
         bin2 = FiniteLineSource.get_bin(lineparams.n2, bins)
-        HM[l][k][i, j] = compute_kernel_line(lineparams; x1=X[bin1], x2=X[bin2], X1=XT[bin1], X2=XT[bin2], P1=PP[bin1], P2=PP[bin2], f1=Fx[bin1], f2=Fx[bin2])
-        HM[l][k][j, i] = HM[l][k][i, j]
+        HMflat[k, i, j] = C * Wflat[k] * compute_kernel_line(lineparams; x1=X[bin1], x2=X[bin2], X1=XT[bin1], X2=XT[bin2], P1=PP[bin1], P2=PP[bin2], f1=Fx[bin1], f2=Fx[bin2])
+        HMflat[k, j, i] = HMflat[k, i, j]
     end
 
-    N, ζ, W, F, expt, expNin, expNout, HM, load_delays, load_buffer
+    qin = zeros(length(ζflat))
+    qout = zeros(length(ζflat))
+
+    N, BlockMethod(ζflat, Fflat, exptflat, expNinflat, expNoutflat, HMflat, load_delays, load_buffer, ranges, Kranges, K_min, qin, qout)
 end
 
-N, ζ, W, F, expt, expNin, expNout, HM, load_delays, load_buffer = @time prepare_containers(bh_positions, D, H, z_eval, ϵ, params, nmodel)
-@info "Precomputation finished"
-#=
-Profile.Allocs.clear()
-@time Profile.Allocs.@profile sample_rate=0.05 prepare_containers(bh_positions, D, H, z_eval, ϵ, params, nmodel)
-PProf.Allocs.pprof(from_c=false)
-=#
+function evolve!(I, q, block::BlockMethod)
+    @unpack ζ, F, expt, expNin, expNout, HM, load_delays, load_buffer, ranges, Kranges, K_min, qinaux, qoutaux = block
 
-#####################################
-# Evolution in time
-#####################################
-Istp = zeros(length(bh_positions), Nt)
+    for (nt, qt) in enumerate(q)
+        enqueue!(load_buffer, qt)
+        current_q = dequeue!(load_buffer)
 
-distances = [compute_distance(source, target) for source in bh_positions, target in bh_positions]
-NR = [σ == 0 ? 0. : compute_N_line(σ, D, H, z_eval, ϵ, params) for σ in distances]
-K_min = [N_r == 0 ? 0 : findlast(x -> x <= N_r, N) for N_r in NR]
+        for i in eachindex(load_delays)
+            qin = current_q
+            enqueue!(load_delays[i], qin)
+            qout = dequeue!(load_delays[i])
+            current_q = qout
+            @. qinaux[ranges[i]] = qin
+            @. qoutaux[ranges[i]] = qout
+        end
+        @. F = expt * F + (qinaux * expNin - qoutaux * expNout) * (1 - expt) / ζ
 
-for i in eachindex(W)
-    for j in eachindex(W[i])
-        HM[i][j] .*= W[i][j]
-    end
-end
-C = 1 / (2π^2*kg)
-
-ζflat = reduce(vcat, ζ)
-Fflat = reduce(vcat, F)
-HMflat = zeros(length(ζflat), length(bh_positions), length(bh_positions))
-i = 1
-for vhm in HM
-    for hm in vhm
-        @. HMflat[i, :, :] = hm
-        i += 1
-    end
-end
-exptflat = reduce(vcat, expt)
-expNinflat = reduce(vcat, expNin)
-expNoutflat = reduce(vcat, expNout)
-
-qinaux = zeros(length(ζflat))
-qoutaux = zeros(length(ζflat))
-
-indices = cumsum([length(F[i]) for i in eachindex(F)])
-insert!(indices, 1, 0)
-ranges = [indices[i]+1:indices[i+1] for i in eachindex(indices[1:end-1])]
-Kranges = [index+1:indices[end] for index in indices[1:end-1]]
-
-@time begin
-for (nt, qt) in enumerate(q)
-    enqueue!(load_buffer, qt)
-    current_q = dequeue!(load_buffer)
-
-    for i in eachindex(F)
-        qin = current_q
-        enqueue!(load_delays[i], qin)
-        qout = dequeue!(load_delays[i])
-        current_q = qout
-        @. qinaux[ranges[i]] = qin
-        @. qoutaux[ranges[i]] = qout
-    end
-    @. Fflat = exptflat * Fflat + C * (qinaux * expNinflat - qoutaux * expNoutflat) * (1 - exptflat) / ζflat
-
-    for target in eachindex(bh_positions)
-        for source in eachindex(bh_positions)
-            if source == target continue end
-            k_min = K_min[source, target]
-            @views Istp[target, nt] += dot(Fflat[Kranges[k_min]], HMflat[Kranges[k_min], target, source])
+        bh_indices = 1:size(block.K_min)[1]
+        for target in bh_indices
+            for source in 1:bh_indices
+                if source == target continue end
+                range = Kranges[K_min[source, target]]
+                @views I[target, nt] += dot(F[range], HM[range, target, source])
+            end
         end
     end
 end
-end
+
+
+N, block = @time prepare_containers(bh_positions, D, H, z_eval, ϵ, params, nmodel)
+Istp = zeros(length(bh_positions), Nt)
+@time evolve!(Istp, q, block)
+
+Profile.clear()
+@profile evolve!(Istp, q, block)
+pprof()
 
 #####################################
 # Validation
@@ -215,42 +204,3 @@ end
 cases = [compute_one_case(source, target) for target in 1:length(bh_positions), source in 1:length(bh_positions)]
 
 compute_one_case(1, 4)
-
-##################
-# Benchmarking
-##################
-# Here I use H-matrices for the computation and storage of the kernels.
-# It is not so useful in terms of total memory size, but it appears to be faster
-@time begin
-for i in eachindex(ζ)
-    for ζζ in ζ[i]
-
-        K = KernelMatrix{Function, Vector{Tuple{Float64, Float64}}, Vector{Tuple{Float64, Float64}}, Float64}(bh_positions, bh_positions) do x, y
-            if x == y return 1. end
-            setup = SegmentToPoint(D=D, H=H, z=z_eval, σ=sqrt((x[1]-y[1])^2 + (x[2]-y[2])^2))
-            lineparams = LineKernelParams(setup, ζζ/rb, ϵ, eval)
-
-            bin1 = FiniteLineSource.get_bin(lineparams.n1, bins)
-            bin2 = FiniteLineSource.get_bin(lineparams.n2, bins)
-
-            compute_kernel_line(lineparams; x1=X[bin1], x2=X[bin2], X1=XT[bin1], X2=XT[bin2], P1=PP[bin1], P2=PP[bin2], f1=F[bin1], f2=F[bin2])
-        end
-    
-        assemble_hmatrix(K, atol=ϵ)
-    end
-end
-end
-
-# Here I store the matrices myself
-@time begin
-function G(x, y, k)
-    if x == y return 1. end
-    setup = SegmentToPoint(D=D, H=H, z=z_eval, σ=sqrt((x[1]-y[1])^2 + (x[2]-y[2])^2))
-    lineparams = LineKernelParams(setup, k/rb, ϵ, eval)
-    bin1 = FiniteLineSource.get_bin(lineparams.n1, bins)
-    bin2 = FiniteLineSource.get_bin(lineparams.n2, bins)
-    compute_kernel_line(lineparams; x1=X[bin1], x2=X[bin2], X1=XT[bin1], X2=XT[bin2], P1=PP[bin1], P2=PP[bin2], f1=F[bin1], f2=F[bin2])
-end
-HM = [[[G(source, target, ζζ) for source in bh_positions, target in bh_positions] for ζζ in ζ[i]] for i in eachindex(ζ)]
-nothing
-end
