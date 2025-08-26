15ae36388ecfc7c05a26daaf7c4fc2bd9f4fc3c4
Marc Basquens <marc5@kth.se>
2025-08-26 13:28:14 +0200

    Fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,8 @@ using Test
 using SpecialFunctions
 
 include("Aqua.jl")
-const ϵ = 5*10^-14
+
+ϵ = 5*10^-14
 
 @testset "point_step_response" begin
     @test point_step_response(3600, 1, 10^-6, 3)       ≈ 0                     atol = ϵ
