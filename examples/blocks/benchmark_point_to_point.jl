bbcdf0378485de8a7e1995a5be8212a5621bd366
Marc Basquens <marc5@kth.se>
2025-09-01 10:54:13 +0200

    Update dependencies

--- examples/blocks/benchmark_point_to_point.jl
+++ examples/blocks/benchmark_point_to_point.jl
@@ -4,16 +4,16 @@ using BenchmarkTools
 
 ϵ = 1e-6
 Δt = 3600.
-Nt = 8760*20
+Nt = 8760
 
 α = 1e-6
 kg = 3.
 rb = 0.1
 constants = Constants(Δt=Δt, α=α, kg=kg, rb=rb)
 
-r = 1.
-
-positions = [PointSource(0., 0., 0., rb), PointSource(r, 0., 0., rb)]
+B = 5.
+#positions = [PointSource(0., 0., 0., rb), PointSource(r, 0., 0., rb)]
+positions = [PointSource(B*i, B*j, B*k, rb) for i in 0:5 for j in 0:5 for k in 0:5]
 q = ones(length(positions), Nt)
 
 #####################################
@@ -26,9 +26,15 @@ block = prepare_containers(setup, positions, ϵ, Nt, constants, compute_first_bl
 Ib = zeros(length(positions), Nt)
 @time evolve!(Ib, q, block)
 
+C = zeros(Nt)
+target = 1
 # Convolution
-C = convolve_step(q[2,:], setup; params=constants)
+for i in 1:length(positions)
+    if target == i continue end
+    r = FiniteLineSource.compute_distance_3D(positions[target], positions[i])
+    C += convolve_step(q[i,:], PointToPoint(r = r); params=constants, ϵ=ϵ/length(positions))
+end
 
 # Error
-err = @. abs(Ib[1, :] - C)
+err = @. abs(Ib[target, :] - C)
 maximum(err)
